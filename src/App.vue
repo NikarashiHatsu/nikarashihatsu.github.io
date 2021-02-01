403d7f83dc240b4a45bb919f042b46251775970c
NikarashiHatsu <yourlovelydev@gmail.com>
2021-02-01 22:52:44 +0700

    feat: added profile

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,30 @@
   <div>
     <Navbar />
     <ThemeToggler />
-    <router-view />
+    <div class="max-w-7xl mx-auto p-6">
+      <div class="grid grid-cols-12 grid-flow-row gap-6">
+        <div class="col-span-9">
+          <router-view />
+        </div>
+        <div class="col-span-3">
+          <Profile />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import ThemeToggler from './components/ThemeToggler.vue'
   import Navbar from './components/Navbar.vue'
+  import Profile from './components/Profile.vue'
+  import ThemeToggler from './components/ThemeToggler.vue'
 
   export default {
     name: 'App',
     components: {
+      Navbar,
+      Profile,
       ThemeToggler,
-      Navbar
     }
   }
 </script>
