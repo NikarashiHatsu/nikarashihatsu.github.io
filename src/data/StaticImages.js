f17a01d0f84e6606eca4319b98e4dfb37d14a628
NikarashiHatsu <yourlovelydev@gmail.com>
2021-02-07 18:59:28 +0700

    feat: added 4reality

--- src/data/StaticImages.js
+++ src/data/StaticImages.js
@@ -1,7 +1,9 @@
 import itwasdaPoldaKaltengAndroid from './../assets/works/thumbnails/itwasda-polda-kalteng-android.webp'
+import fourReality from './../assets/works/thumbnails/4reality.webp'
 
 let staticImages = {
-    itwasdaPoldaKaltengAndroid: itwasdaPoldaKaltengAndroid
+    itwasdaPoldaKaltengAndroid: itwasdaPoldaKaltengAndroid,
+    fourReality: fourReality
 }
 
 export default staticImages
--- src/data/WorksDB.js
+++ src/data/WorksDB.js
@@ -4,12 +4,23 @@ import ItwasdaPoldaKaltengAndroid from './../pages/works/ItwasdaPoldaKaltengAndr
 
 let WorksDB = [
     {
+        slug: undefined,
+        url: 'https://4reality.id',
+        thumbnail: staticImages.fourReality,
+        title: '4 Reality Team Company Profile',
+        overview: 'A company profile for 4 Reality Vtuber Team.',
+        component: undefined,
+        featured: true,
+    },
+    {
         slug: 'itwasda-polda-kalteng-android',
+        url: undefined,
         thumbnail: staticImages.itwasdaPoldaKaltengAndroid,
         title: 'Itwasda Polda Kalteng Android Dashboard',
         overview: 'A dashboard view used for Itwasda Polda Kalimantan Tengah.',
-        component: ItwasdaPoldaKaltengAndroid
-    }
+        component: ItwasdaPoldaKaltengAndroid,
+        featured: false,
+    },
 ]
 
 export default WorksDB
