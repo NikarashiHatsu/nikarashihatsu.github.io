b39b4020f5e242460c57ed00ab2b2fdcb470cdb0
NikarashiHatsu <yourlovelydev@gmail.com>
2023-02-05 22:10:49 +0700

    chore: change vite to hugo

--- src/data/NoteImages.js
+++
@@ -1,7 +0,0 @@
-import sincerelyMedia from './../assets/notes/sincerely-media-nGrfKmtwv24-unsplash.webp'
-
-let notesImages = {
-    artOfRefactoringUI: sincerelyMedia
-}
-
-export default notesImages
--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './main.css'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
